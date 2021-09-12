1f82654695a01e296224ce097e49c422ddb71237
Simon Kolb <dreidekoost@gmail.com>
2021-09-12 13:32:10 +0200

    Route reordering and default route

--- server/update/server.js
+++ server/update/server.js
@@ -5,6 +5,28 @@ const { validateMac } = require('./utils')
 const app = express();
 app.disable('x-powered-by');
 
+app.get('/success/:mac', async (req, res) => {
+  const mac = req.params.mac;
+
+  if (!validateMac(mac)) {
+    log(ip, "invalid");
+    res.status(400).send();
+    return;
+  }
+
+  log(ip, "success", mac);
+
+  checkForDeviceUpdate(mac).then(firmware => {
+    if (firmware) {
+      updateVersion(firmware.type, firmware.id, mac);
+      res.status(204).send();
+      return;
+    }
+    res.status(400).send();
+    return;
+  });
+});
+
 app.get('/:mac', async (req, res) => {
   const ip = req.get("x-real-ip");
   const mac = req.params.mac;
@@ -20,10 +42,10 @@ app.get('/:mac', async (req, res) => {
   // currently all supported devices that have not been registered by hand are
   // Winders with a 1:5 Gear ratio
   deviceExists(mac).then(exists => {
-    if(!exists){
+    if (!exists) {
       registerWinderGearRatio5(mac).then(() => {
         log(ip, "register", mac);
-      });    
+      });
     }
     else log(ip, "check", mac);
   })
@@ -43,26 +65,9 @@ app.get('/:mac', async (req, res) => {
   });
 });
 
-app.get('/success/:mac', async (req, res) => {
-  const mac = req.params.mac;
-
-  if (!validateMac(mac)) {
-    log(ip, "invalid");
-    res.status(400).send();
-    return;
-  }
-
-  log(ip, "success", mac);
-
-  checkForDeviceUpdate(mac).then(firmware => {
-    if (firmware) {
-      updateVersion(firmware.type, firmware.id, mac);
-      res.status(201).send();
-      return;
-    }
-    res.status(400).send();
-    return;
-  });
+// In case no route matches
+app.get('', async (req, res) => {
+  res.status(400).send();
 });
 
 app.listen(5000, () => {
