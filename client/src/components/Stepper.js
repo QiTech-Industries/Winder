23989579603c5e046e1901405f320adaa525a0df
Simon Kolb <dreidekoost@gmail.com>
2021-11-01 17:36:34 +0100

    Integrate with new Stats provider

--- client/src/components/Stepper.js
+++ client/src/components/Stepper.js
@@ -1,38 +1,43 @@
 import { useState } from "preact/hooks"
 import Chart from "./Chart";
+import { useStats } from "../utils/StatsProvider";
 
-const Stepper = ({ name, chart, stats }) => {
+const Stepper = ({ name, state }) => {
     const [showDiagram, setShowDiagram] = useState(false);
-    let rpm = stats.r;
-    if (Math.abs(stats.r) > 100)
-        rpm = stats.r.toFixed(0);
+    const stats = useStats(state => state.stats)
+    const charts = useStats(state => state.charts)
+
+    const status = stats[state];
+    let rpm = status.r;
+    if (Math.abs(status.r) > 100)
+        rpm = status.r.toFixed(0);
     else
-        rpm = stats.r.toFixed(1);
-    const active = stats.a == 0 ? false : true;
+        rpm = status.r.toFixed(1);
+    const active = status.a == 0 ? false : true;
 
     return (
-        <div class={`bg-white py-4 ${showDiagram ? "" : "px-6"} rounded-xl w-full text-center mb-4 relative`}>
-            {
-                showDiagram
-                    ?
-                    <div class="w-full h-full">
-                        <Chart data={chart} />
-                    </div>
-                    :
-                    <>
-                        <h3 class="text-3xl">{name}</h3>
-                        <div class="relative py-2 mx-2">
-                            <svg width="100%" height="100%" viewBox="0 0 194 128" version="1.1" xmlns="http://www.w3.org/2000/svg" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;"><g><rect x="156.077" y="56.808" width="37.021" height="14.043" style="fill:rgb(214,208,204);" /><path d="M157.13,127.659l-0,-127.659l-139.149,0l-0,127.659l139.149,0Z" style="fill:rgb(11,0,0);" /><g><path d="M17.062,81.747c0.507,-0 0.919,-0.412 0.919,-0.919l-0,-33.997c-0,-0.507 -0.412,-0.919 -0.919,-0.919l-7.349,0c-0.507,0 -0.918,0.412 -0.918,0.919l-0,33.997c-0,0.507 0.411,0.919 0.918,0.919l7.349,-0Z" /><path d="M8.795,68.87l-0,-2.541l-8.795,-0l0,2.541l8.795,-0Z" style="fill:rgb(0,219,19);" /><path d="M8.795,75.175l-0,-2.541l-8.795,-0l0,2.541l8.795,0Z" style="fill:rgb(255,63,36);" /><path d="M8.795,61.742l-0,-2.541l-8.795,-0l0,2.541l8.795,0Z" style="fill:rgb(255,217,18);" /><path d="M8.795,55.026l-0,-2.542l-8.795,0l0,2.542l8.795,-0Z" style="fill:rgb(0,118,219);" /></g><path d="M157.13,127.659l-0,-127.659l-14.043,0l0,127.659l14.043,0Z" style="fill:rgb(214,208,204);" /><path d="M32.023,127.659l0,-127.659l-14.042,0l-0,127.659l14.042,0Z" style="fill:rgb(214,208,204);" /><path d="M59.039,108.848c-0,6.243 -5.069,11.312 -11.312,11.312c-6.244,-0 -11.312,-5.069 -11.312,-11.312c-0,-6.244 5.068,-11.312 11.312,-11.312c6.243,-0 11.312,5.068 11.312,11.312Z" class={`${active || rpm != 0 ? "text-green-500" : "text-red-500"} fill-current`} /></g><rect x="35.866" y="31.445" width="103.999" height="39.406" style="fill:white;stroke:rgb(255,0,0);stroke-width:2.5px;" /><text x="40.637px" y="57.59px" style="font-family:'ArialMT', 'Arial', sans-serif;font-size:18px;fill:rgb(255,0,0);">RPM</text><text x="85.973px" y="58.213px" style="font-family:'Arial-BoldMT', 'Arial', sans-serif;font-weight:700;font-size:20px;fill:rgb(255,0,0);">{rpm != 0 ? rpm : "0.000"}</text></svg>
-                        </div>
-                    </>
-
-            }
-            <div class="bg-gray-400 rounded-tl-xl rounded-br-xl h-10 w-10 bottom-0 right-0 absolute cursor-pointer hover:scale-110 transform text-2xl pt-2" onClick={e => setShowDiagram(!showDiagram)}>
+        <div class="w-full py-4">
+            <div class="rounded-t-lg bg-primary w-48 right-0 px-2 py-1 text-center font-bold z-10 h-8">{name}</div>
+            <div class={`bg-white rounded-b-lg rounded-tr-lg text-center h-64 relative`}>
                 {
                     showDiagram
-                        ? <div class="icon-object" />
-                        : <div class="icon-chart" />
+                        ?
+                        <Chart data={charts[state]} />
+                        :
+                        <>
+                            <div class="p-4 h-full">
+                                <svg width="100%" height="100%" viewBox="0 0 309 189" version="1.1" xmlns="http://www.w3.org/2000/svg" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"><g id="Ebene_2"><g id="Ebene_1-2"><path id="Indicator" d="M236.39,148.64l-0,-109.04l9.53,-0l3.14,3.33l0,40.74l54.89,-0l0,20.9l-54.04,-0l0,39.34l-2.85,4.73l-10.67,-0Z" style="fill-rule:nonzero;" class={`${active ? "text-green-700" : "text-red-700"} fill-current`} /><path d="M243.75,152.14l-10.86,-0l0,-116.04l10.86,-0c5.16,0.005 9.405,4.25 9.41,9.41l0,97.22c-0.005,5.16 -4.25,9.405 -9.41,9.41Zm-3.86,-7l3.86,-0c1.322,-0 2.41,-1.088 2.41,-2.41l0,-97.22c-0,-1.322 -1.088,-2.41 -2.41,-2.41l-3.86,-0l0,102.04Z" style="fill-rule:nonzero;" /><path d="M300.87,108.07l-54.49,-0l0,-27.91l54.49,-0c4.416,0.011 8.045,3.644 8.05,8.06l0,11.78c0,0.003 0,0.007 0,0.01c0,4.418 -3.632,8.055 -8.05,8.06Zm-47.49,-7l47.49,-0c0.017,0.001 0.033,0.001 0.05,0.001c0.549,0 1,-0.451 1,-1c0,-0 0,-0.001 0,-0.001l0,-11.85c0,-0.578 -0.472,-1.055 -1.05,-1.06l-47.49,-0l0,13.91Z" style="fill-rule:nonzero;" /><g id="Cable"><rect x="0" y="74.62" width="17.53" height="5.38" /><rect x="0" y="85.4" width="17.53" height="5.37" /><rect x="0" y="95.5" width="17.53" height="5.37" /><rect x="0" y="105.94" width="17.53" height="5.38" /></g><path id="Plug" d="M25.36,67.63l9.64,0l0,53l-9.6,0c-2.107,0 -3.84,-1.733 -3.84,-3.84l0,-45.32c-0,-2.092 1.709,-3.818 3.8,-3.84Z" style="fill-rule:nonzero;" /><g id="Main"><path d="M71.47,188.24l-29.79,0c-4.804,-0.005 -8.755,-3.956 -8.76,-8.76l0,-170.73c0.011,-4.8 3.96,-8.745 8.76,-8.75l29.79,-0l0,188.24Zm-29.79,-181.24c-0.962,-0 -1.755,0.788 -1.76,1.75l0,170.73c0,0.966 0.794,1.76 1.76,1.76l22.79,-0l0,-174.24l-22.79,0Z" style="fill-rule:nonzero;" /><path d="M230.1,188.24l-29.79,0l0,-188.24l29.79,0c4.8,0.005 8.749,3.95 8.76,8.75l0,170.73c-0.005,4.804 -3.956,8.755 -8.76,8.76Zm-22.79,-7l22.79,0c0.966,0 1.76,-0.794 1.76,-1.76l0,-170.73c-0.005,-0.962 -0.798,-1.75 -1.76,-1.75l-22.79,0l0,174.24Z" style="fill-rule:nonzero;" /><path d="M207.31,182.46l-142.84,0l0,-176.69l142.84,-0l0,176.69Zm-135.84,-7l128.84,0l0,-162.69l-128.84,-0l0,162.69Z" style="fill-rule:nonzero;" /></g><g id="RPM"><text x="106.85px" y="87.44px" style="font-family:'Nunito', 'Arial', sans-serif;font-size:26.19px;">RPM</text><text x="134px" y="118.86px" text-anchor="middle" style="font-family:'Nunito', 'Arial', sans-serif;font-size:26.19px;">{rpm != 0 ? rpm : "0.000"}</text></g><g id="Lines_B"><path d="M90,182.22c-2.467,-0.005 -4.495,-2.033 -4.5,-4.5l0,-20.3c0,-2.469 2.031,-4.5 4.5,-4.5c2.469,0 4.5,2.031 4.5,4.5l0,20.3c0,2.469 -2.031,4.5 -4.5,4.5Z" style="fill-rule:nonzero;" /><path d="M112.66,182.22c-2.467,-0.005 -4.495,-2.033 -4.5,-4.5l0,-20.3c0,-2.469 2.031,-4.5 4.5,-4.5c2.469,0 4.5,2.031 4.5,4.5l0,20.3c0,2.469 -2.031,4.5 -4.5,4.5Z" style="fill-rule:nonzero;" /><path d="M135.29,182.22c-2.467,-0.005 -4.495,-2.033 -4.5,-4.5l0,-20.3c0,-2.469 2.031,-4.5 4.5,-4.5c2.469,0 4.5,2.031 4.5,4.5l0,20.3c0,2.469 -2.031,4.5 -4.5,4.5Z" style="fill-rule:nonzero;" /><path d="M157.92,182.22c-2.467,-0.005 -4.495,-2.033 -4.5,-4.5l-0,-20.3c-0,-2.469 2.031,-4.5 4.5,-4.5c2.469,0 4.5,2.031 4.5,4.5l-0,20.3c-0,2.469 -2.031,4.5 -4.5,4.5Z" style="fill-rule:nonzero;" /><path d="M180.55,182.22c-2.467,-0.005 -4.495,-2.033 -4.5,-4.5l0,-20.3c0,-2.469 2.031,-4.5 4.5,-4.5c2.469,0 4.5,2.031 4.5,4.5l0,20.3c0,2.469 -2.031,4.5 -4.5,4.5Z" style="fill-rule:nonzero;" /></g><g id="Lines_T"><path d="M90,35.33c-2.467,-0.005 -4.495,-2.033 -4.5,-4.5l0,-19c0,-2.469 2.031,-4.5 4.5,-4.5c2.469,-0 4.5,2.031 4.5,4.5l0,19c0,2.469 -2.031,4.5 -4.5,4.5Z" style="fill-rule:nonzero;" /><path d="M112.66,35.33c-2.467,-0.005 -4.495,-2.033 -4.5,-4.5l0,-19c0,-2.469 2.031,-4.5 4.5,-4.5c2.469,-0 4.5,2.031 4.5,4.5l0,19c0,2.469 -2.031,4.5 -4.5,4.5Z" style="fill-rule:nonzero;" /><path d="M135.29,35.33c-2.467,-0.005 -4.495,-2.033 -4.5,-4.5l0,-19c0,-2.469 2.031,-4.5 4.5,-4.5c2.469,-0 4.5,2.031 4.5,4.5l0,19c0,2.469 -2.031,4.5 -4.5,4.5Z" style="fill-rule:nonzero;" /><path d="M157.92,35.33c-2.467,-0.005 -4.495,-2.033 -4.5,-4.5l-0,-19c-0,-2.469 2.031,-4.5 4.5,-4.5c2.469,-0 4.5,2.031 4.5,4.5l-0,19c-0,2.469 -2.031,4.5 -4.5,4.5Z" style="fill-rule:nonzero;" /><path d="M180.55,35.33c-2.467,-0.005 -4.495,-2.033 -4.5,-4.5l0,-19c0,-2.469 2.031,-4.5 4.5,-4.5c2.469,-0 4.5,2.031 4.5,4.5l0,19c0,2.469 -2.031,4.5 -4.5,4.5Z" style="fill-rule:nonzero;" /></g></g></g></svg>
+                            </div>
+                        </>
+
                 }
+                <div class="rounded-lg h-10 w-10 bottom-2 right-2 absolute cursor-pointer hover:scale-110 transform text-2xl pt-2 text-primary bg-ternary" onClick={e => setShowDiagram(!showDiagram)}>
+                    {
+                        showDiagram
+                            ? <div class="icon-object" />
+                            : <div class="icon-chart" />
+                    }
+                </div>
             </div>
         </div>
     )
--- client/src/components/Winder.js
+++ client/src/components/Winder.js
@@ -1,53 +1,52 @@
-import Menu from "./Menu"
 import Button from "./Button"
 import Stepper from "./Stepper"
 import Spool from "./Spool"
-import { useState, useEffect } from "preact/hooks"
+import { useState } from "preact/hooks"
 import { useSocket } from '../utils/SocketProvider';
-import { useStats } from '../utils/StatsProvider';
+import { useStats } from "../utils/StatsProvider";
 import Slider from "./Slider"
 
 const Winder = () => {
     const [speed, setSpeed] = useState(3);
-
+    
     const { socket } = useSocket();
-    const { stats, charts } = useStats();
+    const mode = useStats(state => state.stats.m)
 
     const menu = [
         {
             name: "Start Puller",
             action: () => socket.emit("pull", { mpm: speed }),
-            active: stats.m != "pulling" && stats.m != "winding"
+            active: mode != "pulling" && mode != "winding"
         },
         {
             name: "Start Winding",
             action: () => socket.emit("wind", { mpm: speed }),
-            active: stats.m == "pulling"
+            active: mode == "pulling"
         },
         {
             name: "Winding",
             action: () => socket.emit("power"),
-            active: stats.m == "winding"
+            active: mode == "winding"
         },
         {
             name: "Unwind",
             action: () => socket.emit("unwind", { mpm: speed }),
-            active: stats.m != "unwinding"
+            active: mode != "unwinding"
         },
         {
             name: "Unwinding",
             action: () => socket.emit("power"),
-            active: stats.m == "unwinding"
+            active: mode == "unwinding"
         },
         {
             name: "Off",
             action: () => socket.emit("standby"),
-            active: stats.m != "standby"
+            active: mode != "standby"
         },
         {
             name: "Power",
             action: () => socket.emit("power"),
-            active: stats.m == "standby"
+            active: mode == "standby"
         }
     ];
 
@@ -57,33 +56,42 @@ const Winder = () => {
     }
 
     return (
-        <div class="h-full text-black mx-4">
-            <div class="py-2 w-full md:w-1/2 z-50 sticky top-0">
-                <Menu enabled={stats.m != "standby"}>
-                    {
-                        menu.map(item => {
-                            if (item.active) {
-                                return <Button onClick={e => item.action()}>
-                                    {item.name}
-                                </Button>
-                            }
-                        })
-                    }
-                </Menu>
-            </div>
-            <div class="max-w-6xl mx-auto">
+        <>
+            <div class="max-w-6xl mx-auto py-4 px-4">
                 <div class="flex flex-wrap items-center justify-around md:flex-nowrap md:space-x-8">
-                    <Stepper stats={stats.p} chart={charts.p} name="Puller" />
-                    <Stepper stats={stats.f} chart={charts.f} name="Ferrari" />
-                    <Stepper stats={stats.s} chart={charts.s} name="Spool" />
+                    <Stepper state="p" name="Puller" />
+                    <Stepper state="f" name="Ferrari" />
+                    <Stepper state="s" name="Spool" />
                 </div>
-                <div class="col-span-3 w-full text-center py-3">
-                    <h3 class="text-white text-2xl pb-2">Speed Adjust</h3>
-                    <Slider min={0.5} max={14} minThumb={speed} step={0.1} onChange={mpm => changeSpeed(mpm)} value={speed} />
+                <div class="w-full text-center my-4">
+                    <div class="rounded-t-lg w-48 right-0 px-2 py-1 bg-primary font-bold">Speed Adjust</div>
+                    <div class="w-full bg-white rounded-lg rounded-tl-none px-2 py-3 shadow-inner">
+                        <Slider min={0.5} max={14} minThumb={speed} step={0.1} onChange={mpm => changeSpeed(mpm)} />
+                    </div>
                 </div>
                 <Spool />
+            </div >
+            <div class="sticky bottom-0 w-full bg-white z-50">
+                <div class="w-full h-2 bg-primary"></div>
+                <div class="w-full border-t-4">
+                    <div class="flex mx-auto px-4 my-4 space-x-4 justify-center">
+                        {
+                            menu.map(item => {
+                                if (item.active) {
+                                    return (
+                                        <div class="w-36">
+                                            <Button secondary onClick={e => item.action()}>
+                                                {item.name}
+                                            </Button>
+                                        </div>
+                                    )
+                                }
+                            })
+                        }
+                    </div>
+                </div>
             </div>
-        </div >
+        </>
     )
 }
 
