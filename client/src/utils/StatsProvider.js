d72d10ffa63f48e44556beffed8c6aa403718cf3
Simon Kolb <dreidekoost@gmail.com>
2021-11-01 17:06:32 +0100

    Switch Stats Provider to Zustand

--- client/src/utils/StatsProvider.js
+++ client/src/utils/StatsProvider.js
@@ -1,78 +1,55 @@
-import { createContext } from 'preact';
-import { useContext, useEffect, useState, useRef } from 'preact/hooks'
-import { useSocket } from './SocketProvider';
+import create from "zustand"
 
-const StatsContext = createContext();
-
-export const useStats = () => {
-    return useContext(StatsContext);
-}
-
-export const StatsProvider = ({ children }) => {
-    const { socket } = useSocket();
-    const [charts, setCharts] = useState({
-        p: [],
-        s: [],
-        f: []
-    });
-
-    const stats = useRef({
-        s: {//spool
-            r: 0,//rpm
-            s: 0,//stall
-            a: false,//active
+export const useStats = create((set, get) => {
+    return {
+        charts: {
+            p: [],
+            s: [],
+            f: [],
+            update: () => set(prev => {
+                return {
+                    charts: {
+                        ...prev.charts,
+                        p: [prev.stats.p.r, ...prev.charts.p].slice(0, 50),
+                        s: [prev.stats.s.r, ...prev.charts.s].slice(0, 50),
+                        f: [prev.stats.f.r, ...prev.charts.f].slice(0, 50)
+                    },
+                    stats: {
+                        // Increment timer while winding, Reset timer if windings are reset
+                        ...prev.stats,
+                        t: prev.stats.m == "winding" ?
+                            prev.stats.t + 1 :
+                            (prev.stats.w == 0 ? 0 : prev.stats.t)
+                    }
+                }
+            }),
         },
-        p: {//puller
-            r: 0,
-            s: 0,
-            a: false,
-        },
-        f: {//ferrari
-            r: 0,
-            s: 0,
-            a: false,
-        },
-        m: "standby",//mode
-        w: 0, // total windings
-        l: 0, // total length
-        t: 0, // time
-        e: null//error
-    });
+        stats: {
+            s: {//spool
+                r: 0,//rpm
+                s: 0,//stall
+                a: false,//active
+            },
+            p: {//puller
+                r: 0,
+                s: 0,
+                a: false,
+            },
+            f: {//ferrari
+                r: 0,
+                s: 0,
+                a: false,
+            },
+            m: "standby",//mode
+            w: 0, // total windings
+            l: 0, // total length
+            t: 0, // time
+            e: null, //error
+            update: (next) => set(prev => ({stats: {...next, t: prev.stats.t}}))
+        }
+    }
+});
 
-    useEffect(() => {
-        if (!socket) return;
-        socket.on("stats", data => {
-            data.t = stats.current.t;
-            stats.current = data;
-        });
-    }, []);
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            // Update chart data every second
-            const newCharts = {};
-
-            newCharts.p = [stats.current.p.r, ...charts.p].slice(0, 50);
-            newCharts.f = [stats.current.f.r, ...charts.f].slice(0, 50);
-            newCharts.s = [stats.current.s.r, ...charts.s].slice(0, 50);
-
-            setCharts(newCharts);
-
-            // Increment timer while winding
-            if (stats.current.m == "winding") {
-                stats.current.t++;
-            }
-            // Reset timer if windings are reset
-            else if (stats.current.w == 0) {
-                stats.current.t = 0;
-            }
-        }, 1000);
-        return () => { clearInterval(interval) };
-    }, [charts])
-
-    return (
-        <StatsContext.Provider value={{ stats: stats.current, charts }}>
-            {children}
-        </StatsContext.Provider>
-    )
-}
+setInterval(() => {
+    useStats.getState().charts.update();
+}, 1000);
