a875ccba8cf344083e3693f53fda36e671d46516
Simon Kolb <dreidekoost@gmail.com>
2022-02-16 17:22:54 +0100

    Colored Serial prints example

---
+++ esp/examples/ColoredSerial/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+void setup () {
+Serial.begin(11520);
+Serial.println("\033[0;37;40m Normal text\n");
+Serial.println("\033[2;37;40m Underlined text\033[0;37;40m \n");
+Serial.println("\033[1;37;40m Bright Colour\033[0;37;40m \n");
+Serial.println("\033[3;37;40m Negative Colour\033[0;37;40m \n");
+Serial.println("\033[5;37;40m Negative Colour\033[0;37;40m\n");
+ 
+Serial.println("\033[1;37;40m \033[2;37:40m TextColour BlackBackground          TextColour GreyBackground                WhiteText ColouredBackground\033[0;37;40m\n");
+Serial.println("\033[1;30;40m Dark Gray      \033[0m 1;30;40m            \033[0;30;47m Black      \033[0m 0;30;47m               \033[0;37;41m Black      \033[0m 0;37;41m");
+Serial.println("\033[1;31;40m Bright Red     \033[0m 1;31;40m            \033[0;31;47m Red        \033[0m 0;31;47m               \033[0;37;42m Black      \033[0m 0;37;42m");
+Serial.println("\033[1;32;40m Bright Green   \033[0m 1;32;40m            \033[0;32;47m Green      \033[0m 0;32;47m               \033[0;37;43m Black      \033[0m 0;37;43m");
+Serial.println("\033[1;33;40m Yellow         \033[0m 1;33;40m            \033[0;33;47m Brown      \033[0m 0;33;47m               \033[0;37;44m Black      \033[0m 0;37;44m");
+Serial.println("\033[1;34;40m Bright Blue    \033[0m 1;34;40m            \033[0;34;47m Blue       \033[0m 0;34;47m               \033[0;37;45m Black      \033[0m 0;37;45m");
+Serial.println("\033[1;35;40m Bright Magenta \033[0m 1;35;40m            \033[0;35;47m Magenta    \033[0m 0;35;47m               \033[0;37;46m Black      \033[0m 0;37;46m");
+Serial.println("\033[1;36;40m Bright Cyan    \033[0m 1;36;40m            \033[0;36;47m Cyan       \033[0m 0;36;47m               \033[0;37;47m Black      \033[0m 0;37;47m");
+Serial.println("\033[1;37;40m White          \033[0m 1;37;40m            \033[0;37;40m Light Grey \033[0m 0;37;40m               \033[0;37;48m Black      \033[0m 0;37;48m");
+}
+
+void loop(){}
